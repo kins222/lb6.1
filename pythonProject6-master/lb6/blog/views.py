9b00d845504a807e2724f49e36d587a16e7e9e94
TheVispan <theaveragesiren@gmail.com>
2022-09-15 13:32:30 +0300

    rrr

--- pythonProject6-master/lb6/blog/views.py
+++ pythonProject6-master/lb6/blog/views.py
@@ -1,12 +1,22 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
+from django.views.generic.edit import CreateView, UpdateView
 
 class BlogListView(ListView):
     model = Post
-    template_name='home.html'
+    template_name = 'home.html'
 
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
 # Create your views here.
